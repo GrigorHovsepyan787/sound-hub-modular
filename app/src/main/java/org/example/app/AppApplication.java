95509057289f42c1c20d53359e715bc060d949c2
Grigor Hovsepyan <theo.grigor787@gmail.com>
2026-03-03 22:00:30 +0400

    Main page front/fragments

--- app/src/main/java/org/example/app/AppApplication.java
+++ app/src/main/java/org/example/app/AppApplication.java
@@ -2,8 +2,14 @@ package org.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
+@EntityScan(basePackages = {"com.example.model"})
+@EnableJpaRepositories(basePackages = {"com.example.repository"})
 public class AppApplication {
 
     public static void main(String[] args) {
