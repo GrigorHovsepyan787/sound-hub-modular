bb14dcc436aeea5f32ff7564e9f981a3e34583b2
Grigor Hovsepyan <theo.grigor787@gmail.com>
2026-04-12 02:18:17 +0400

    Mailing, improvements in login and register pages, new necessary templates

---
+++ common/src/main/java/com/example/dto/SongPopularityDto.java
@@ -0,0 +1,19 @@
+package com.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class SongPopularityDto {
+
+    private SongDto song;
+
+    private long playCount;
+
+}
