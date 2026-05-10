b34ad517e8c666b19f5feba49dd8602a49a076a4
Grigor Hovsepyan <theo.grigor787@gmail.com>
2026-05-10 04:47:56 +0400

    fix

--- persistence/src/main/java/com/example/repository/UserRepository.java
+++ persistence/src/main/java/com/example/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.example.repository;
 
 import com.example.model.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -14,8 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    @Transactional
-    @Modifying
     Page<User> findAll(Specification<User> spec, Pageable pageable);
 
     long countByRegistrationDateBetween(LocalDateTime start, LocalDateTime end);
